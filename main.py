258b80b8a85a3bdda634b90ab628101899c66eaf
shiduyule <48233680+shiduyule@users.noreply.github.com>
2022-11-14 21:30:11 +0800

    modified main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-#%% 导入模块 import modules
+#%% import modules
 import matplotlib.pyplot as plt  
 import numpy as np
-
-
-#%% 矩阵输入 matrix input
+#=============================================================================
+#%%  矩阵输入 matrix input
 # =============================================================================
-# 利用numpy中的二维数组作为容器  首先手动输入光轴倾角的数据  10 * 10 
+# 利用numpy中的二维数组作为容器  首先手动输入数据  矩阵尺寸任意 以5 * 5为例
+# Use 2D arrays in numpy as containers.First enter the data manually,the matrix size is arbitrary 
 # =============================================================================
-
 data2d = np.array([
 [1,   0.5, 0,  0.5, 1],
 [0.8, 0.5, 0,  0.5, 0.8],
@@ -16,18 +15,17 @@ data2d = np.array([
 [0.8, 0.5, 0,  0.5, 0.8], 
 [1,   0.5, 0,  0.5, 1]  ])
 
-# 结果为二维数组
-# data2d = np.random.random((10,10))
- 
-
-
-#%% 矩阵扩充  matrix expand 至少扩充到 100 * 100
+#=============================================================================
+#%% 矩阵扩充  matrix enhancement  
+#=============================================================================
 benzhenghangshu = np.shape(data2d)[0]  #行数  
 benzhenglieshu = np.shape(data2d)[1]  #列数
 #=============================================================================
-# 扩充列数  设置density 即可
+# 扩充列数 Increase the number of columns 
+# Here,density is a parameter used to modify resolution of colums,you can change it as you need.
+#=============================================================================
 j = 0
-density = 10  # 插入的矩阵行数为data2d的行数  列数为设置的密度值
+density = 100  
 while True: 
     insertcolumn = np.empty([np.shape(data2d)[0] ,density], dtype = float)
     i = 0
@@ -43,10 +41,11 @@ while True:
     if muqianlieshu >= ((benzhenglieshu-1)*density + benzhenglieshu):
         break
 #============================================================================
-#扩充行数
+#扩充行数 Increase the number of rows 
+#Here,density is a parameter used to modify resolution of colums,you can change it as you need.
+#============================================================================
 i = 0
-density = 10 
-# 插入的矩阵行数为data2d的行数  列数为设置的密度值
+density = 10
 while True:
     insertline = np.empty([density,np.shape(data2d)[1]], dtype = float)
     j = 0   # 从第1列开始
@@ -59,32 +58,17 @@ while True:
     muqianhangshu = np.shape(data2d)[0]  # 目前行数
     if muqianhangshu == (benzhenghangshu-1)*density + benzhenghangshu:
         break
-# #%% 矩阵转化 利用matlab中 拟合得到的函数
-# def function_theta_strain(x):
-#     a0 =   7.363e-05 
-#     a1 =  -8.187e-05 
-#     b1 =  -2.305e-05 
-#     a2 =   7.352e-06 
-#     b2 =   1.373e-05 
-#     a3 =   9.547e-07 
-#     b3 =  -1.768e-06 
-#     w =       1.798  
-#     S21output =  a0 + a1 * np.cos(x*w) + b1 * np.sin(x*w) + a2 * np.cos(2*x*w) + b2 * np.sin(2*x*w) + a3 * np.cos(3*x*w) + b3 * np.sin(3*x*w)
-#     return (S21output)
-
-# data2d = function_theta_strain(data2d)
-
-
-
-#%% 创建画布
+      
+# =============================================================================
+#%% 创建画布 create figure 
+# =============================================================================
 fig, ax = plt.subplots(facecolor='#F5F5EB')  # 等价于 plt.subplots(1,1) 创建画布fig 和一个子区域
-
 im = ax.imshow(data2d)          
 
-#%% 追加图例
+# =============================================================================
+#%% 追加图例 set title and colorbar
+# =============================================================================
 ax.set_title('Pan on the colorbar to shift the color mapping\n'
              'Zoom on the colorbar to scale the color mapping')
-
 fig.colorbar(im, ax=ax, label='Interactive colorbar')  
-
 plt.show()
